7aa35fd234ab17f120133c1d0a84cbb9791dbc4f
agguan <andrewg1153@gmail.com>
2024-07-21 03:03:06 -0400

    added SuperCoin.css

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import './SuperCoin.css'; 
 
 const SuperCoin = () => {
   const [superCoins, setSuperCoins] = useState(0);
